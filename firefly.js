3df455744ddcb509609fa1cdcf69f98e90e88bd2
ornatwork <ornsmail@gmail.com>
2015-01-19 14:14:40 -0700

    collition adjustment

--- firefly.js
+++ firefly.js
@@ -1,26 +1,26 @@
 
 // Globals
-var 
-  xMax = 300, 
+var
+  xMax = 300,
   yMax = 300,
   zMax = 300,
   collideZone = 20;
   flies = null,
   ctx = null
-  
+
 
 // Main function
-function run() 
+function run()
 {
-	
-	 function Fly() 
-	 { 
+
+	 function Fly()
+	 {
 	    this.x=0;
 	    this.y=0;
 	    this.z=0;
 	    this.blink=0;
 	 }
-	
+
 	 Fly.prototype.paint = function ()
 	 {
       // Randomize the direction
@@ -33,18 +33,25 @@ function run()
       tmp = this.z + Math.floor((Math.random()*(10+10+1)-10));
       if( tmp > 0 && tmp < zMax ) this.z = tmp;
 
-      //  blinks every x times 
+      //  blinks every x times
       this.blink = Math.floor((Math.random()*300));
+
+      // Size is portional to dept
+      var size = (this.z / 10);
+      // Adjust the collition zone as well
+      var adjustCollideZone = collideZone;
+      if( size < collideZone)
+        adjustCollideZone = collideZone - size;
 
       // Loop the flies from the array for collection detection
       var collision = false;
-      for (var i = 0; i < flies.length; i++) 
+      for (var i = 0; i < flies.length; i++)
       {
         // Can't collide with itself
         if( flies[i] != this )
-          if( flies[i].x - this.x > -collideZone && flies[i].x - this.x < collideZone &&
-              flies[i].y - this.y > -collideZone && flies[i].y - this.y < collideZone &&
-              flies[i].z - this.z > -collideZone && flies[i].z - this.z < collideZone )
+          if( flies[i].x - this.x > -adjustCollideZone && flies[i].x - this.x < adjustCollideZone &&
+              flies[i].y - this.y > -adjustCollideZone && flies[i].y - this.y < adjustCollideZone &&
+              flies[i].z - this.z > -adjustCollideZone && flies[i].z - this.z < adjustCollideZone )
                 collision = true
       }
 
@@ -55,21 +62,19 @@ function run()
         ctx.strokeStyle = ctx.fillStyle = '#CC0000';
       else
         ctx.strokeStyle = ctx.fillStyle = '#00CC00';
-      
-      // Size is portional to dept
-      var size = (this.z / 10);
+
 
       // Left wing
       ctx.moveTo(this.x, this.y);
       ctx.arc( this.x, this.y, size,
             Math.PI * 0.9, Math.PI * 1.1 );
-      ctx.lineTo(this.x, this.y); 
+      ctx.lineTo(this.x, this.y);
 
       // Right wing
       ctx.moveTo(this.x, this.y);
       ctx.arc( this.x, this.y, size,
             Math.PI * 1.9, Math.PI * 0.1 );
-      ctx.lineTo(this.x, this.y); 
+      ctx.lineTo(this.x, this.y);
       // Finish off the wings
       ctx.stroke();
 
@@ -82,38 +87,38 @@ function run()
       ctx.moveTo(this.x, this.y-bodyStartOffset);
       ctx.arc( this.x, this.y, size,
             Math.PI * 0.4, Math.PI * 0.6 );
-      ctx.lineTo(this.x, this.y-bodyStartOffset); 
+      ctx.lineTo(this.x, this.y-bodyStartOffset);
 
-      // blink once in a while 
+      // blink once in a while
       if( this.blink % 75 == 0 )
         ctx.fill();
 
-      // Finish off the body      
+      // Finish off the body
       ctx.stroke();
 	  }
 
-    Fly.prototype.publicMethod = function () 
+    Fly.prototype.publicMethod = function ()
     {
       alert(this.x);
     };
-    Fly.prototype.publicMethod = function () 
+    Fly.prototype.publicMethod = function ()
     {
       alert(this.y);
     };
-    Fly.prototype.publicMethod = function () 
+    Fly.prototype.publicMethod = function ()
     {
       alert(this.z);
     };
-    Fly.prototype.publicMethod = function () 
+    Fly.prototype.publicMethod = function ()
     {
       alert(this.blink);
     };
-    Fly.prototype.publicMethod = function () 
+    Fly.prototype.publicMethod = function ()
     {
       alert(this.paint);
     };
-	
-    // Get a hold of the context 
+
+    // Get a hold of the context
     ctx = document.getElementById('scene').getContext('2d');
     xMax = ctx.canvas.width  = window.innerWidth-25;
     yMax = ctx.canvas.height = window.innerHeight-25;
@@ -128,18 +133,18 @@ function run()
       flies[i].z = (Math.random()*zMax);
     }
 
-    // 
+    //
     renderFrame();
   }
 
-  // Render  
-  function renderFrame() 
+  // Render
+  function renderFrame()
   {
-	  // clear  
+	  // clear
     ctx.clearRect(0, 0, xMax, yMax);
-    
-    // Loop the flies from the array 
-    for (var i = 0; i < flies.length; i++) 
+
+    // Loop the flies from the array
+    for (var i = 0; i < flies.length; i++)
       flies[i].paint();
 
     // Call me
